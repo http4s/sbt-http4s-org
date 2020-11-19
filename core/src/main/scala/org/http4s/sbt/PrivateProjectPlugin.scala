83fac33dfd2ede21f39b2c6629e24fcb33c78e07
Ross A. Baker <ross@rossabaker.com>
2020-11-18 21:05:06 -0500

    Don't run sbt-explicit-dependencies on private projects

--- core/src/main/scala/org/http4s/sbt/PrivateProjectPlugin.scala
+++ core/src/main/scala/org/http4s/sbt/PrivateProjectPlugin.scala
@@ -4,15 +4,25 @@ import com.typesafe.tools.mima.plugin.MimaPlugin
 import com.typesafe.tools.mima.plugin.MimaPlugin.autoImport.mimaPreviousArtifacts
 import sbt._
 import sbt.Keys._
+import explicitdeps.ExplicitDepsPlugin
+import explicitdeps.ExplicitDepsPlugin.autoImport.{
+  undeclaredCompileDependenciesTest,
+  unusedCompileDependenciesTest
+}
 
 object PrivateProjectPlugin extends AutoPlugin {
   override def trigger = noTrigger
 
-  override def requires = Http4sOrgPlugin && MimaPlugin
+  override def requires =
+    Http4sOrgPlugin &&
+      MimaPlugin &&
+      ExplicitDepsPlugin
 
   override lazy val projectSettings: Seq[Setting[_]] =
     Seq(
       publish / skip := true,
-      mimaPreviousArtifacts := Set.empty
+      mimaPreviousArtifacts := Set.empty,
+      undeclaredCompileDependenciesTest := {},
+      unusedCompileDependenciesTest := {}
     )
 }
