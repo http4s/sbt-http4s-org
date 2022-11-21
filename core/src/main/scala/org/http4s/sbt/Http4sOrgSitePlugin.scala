55c4dde550aad227e479c84b190fc4209360d273
Arman Bilge <armanbilge@gmail.com>
2022-11-21 06:20:01 -0800

    Fix formatting

--- core/src/main/scala/org/http4s/sbt/Http4sOrgSitePlugin.scala
+++ core/src/main/scala/org/http4s/sbt/Http4sOrgSitePlugin.scala
@@ -107,7 +107,7 @@ object Http4sOrgSitePlugin extends AutoPlugin {
               scmInfo.value.fold("https://github.com/http4s")(_.browseUrl.toString),
               HeliumIcon.github,
               options = Styles("svg-link")),
-            IconLink.external("https://discord.gg/XF3CXcMzqD", HeliumIcon.chat),
+            IconLink.external("https://discord.gg/XF3CXcMzqD", HeliumIcon.chat)
           )
         )
     }
